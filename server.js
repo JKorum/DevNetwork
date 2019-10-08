f601986d177562c83f8cd86905c227806320a33c
JKorum <constantine.proshin@gmail.com>
2019-10-08 13:50:11 +0300

    add test routes

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 const express = require('express')
 const connectToMongo = require('./config/db')
+
+//connect to database
 connectToMongo()
 
+const PORT = process.env.PORT || 3015
 const app = express()
 
-const PORT = process.env.PORT || 3015
-
-app.get('/', (req, res) => {
-  res.send('all is ok')
-})
+//define routes
+app.use('/api/users', require('./routes/api/users'))
+app.use('/api/auth', require('./routes/api/auth'))
+app.use('/api/profile', require('./routes/api/profile'))
+app.use('/api/posts', require('./routes/api/posts'))
 
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`)
