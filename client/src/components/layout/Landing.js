f767e32920f642cf6c45a34a103f111807870e74
JKorum <constantine.proshin@gmail.com>
2019-11-03 13:13:48 +0300

    add dynamic landing background

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link, Redirect } from 'react-router-dom'
 import { connect } from 'react-redux'
+import video from '../../media/background.mp4'
 
 const Landing = ({ isAuthenticated }) => {
   if (isAuthenticated) {
@@ -9,6 +10,8 @@ const Landing = ({ isAuthenticated }) => {
   return (
     <section className='landing'>
       <div className='dark-overlay'>
+        <video src={video} autoPlay loop></video>
+
         <div className='landing-inner'>
           <h1 className='x-large'>Developer Network ^1.0.2</h1>
           <p className='lead'>
@@ -34,3 +37,27 @@ const mapStateToProps = state => ({
 })
 
 export default connect(mapStateToProps)(Landing)
+
+{
+  /* <section className='landing'>
+      <div className='dark-overlay'>
+        <video src={video}></video>
+
+        <div className='landing-inner'>
+          <h1 className='x-large'>Developer Network ^1.0.2</h1>
+          <p className='lead'>
+            The place for landing your knowledge portfolio, collaboration and
+            fetching feedback
+          </p>
+          <div className='buttons'>
+            <Link to='/register' className='btn btn-primary'>
+              Sign Up
+            </Link>
+            <Link to='/login' className='btn'>
+              Login
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section> */
+}
